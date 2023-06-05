67d24aa42dd497fa2c4c51a3ea7a1fd4a8ff8303
Chaahat <arorac2@mymacewan.ca>
2023-06-05 02:26:12 -0600

    add modal for login

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,12 @@
 // Client facing scripts here
+const loginButton = document.querySelector('.button-container .button');
+const modal = document.querySelector('#modal');
+const closeButton = document.querySelector('#closeButton');
+
+loginButton.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+closeButton.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
