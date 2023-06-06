fb980dac1a3727c5207bf2563b1f4b94b833ff30
Chaahat <arorac2@mymacewan.ca>
2023-06-06 01:00:20 -0600

    finish modal styling

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,33 +1,32 @@
 // Client facing scripts here
-const loginButton = document.querySelector('.button-container .button');
-const modal = document.querySelector('#modal');
-const closeButton = document.querySelector('#closeButton');
+const loginButton = document.querySelector(".button-container .button");
+const modal = document.querySelector("#modal");
+const closeButton = document.querySelector("#closeButton");
 
-loginButton.addEventListener('click', () => {
-  modal.style.display = 'block';
-  removeLoginErrorMessage();
+loginButton.addEventListener("click", () => {
+  modal.style.display = "block";
 });
 
-// document.addEventListener('click', function(event {
-// let modal = document.getElementById('modal');
+$(document).ready(function() {
+  const modalClose = document.getElementById('modal'); // Replace 'yourModalId' with the ID of your modal element
 
-// //is the clicked element is outside the modal?
-// if (!modal.container(event.target)) {
-// modal.style.display = 'none'
-// }
-// });
+  // Close the modal when clicking outside of it
+  $(document).on('click', function(event) {
+    if (event.target === modalClose) {
+      modalClose.style.display = 'none';
+    }
+  });
+});
 
-
-
-const printResult = function(selector) {
+const printResult = function (selector) {
   let text = $(selector).val();
   let count = 140 - text.length;
-  $('.counter').text(count);
-  $('.counter').toggleClass('negative', count < 0);
+  $(".counter").text(count);
+  $(".counter").toggleClass("negative", count < 0);
 };
 
-const populateTable = function(data) {
-  const tableBody = $('tbody');
+const populateTable = function (data) {
+  const tableBody = $("tbody");
 
   for (const item of data) {
     const row = `
@@ -46,8 +45,6 @@ const populateTable = function(data) {
 };
 
 const aiForm = (formData) => {
-
-
   const resultForm = `
   <div class="container">
     <form id="userInteraction">
@@ -94,89 +91,89 @@ const aiForm = (formData) => {
   }, 300);
 
   const ajaxRequest = $.ajax({
-    url: '/api/open-ai',
-    type: 'POST',
+    url: "/api/open-ai",
+    type: "POST",
     data: formData,
-  }).done((response) => {
+  })
+    .done((response) => {
+      const item = response.item;
+      const description = response.description;
+      console.log("response", response.item);
+      const cId = response["categoryId"];
+      console.log("cid1: ", cId);
 
-    const item = response.item;
-    const description = response.description;
-    console.log("response", response.item);
-    const cId = response['categoryId'];
-    console.log("cid1: ", cId);
+      $("#loading-ai").remove();
 
-    $('#loading-ai').remove();
+      console.log("prepending");
 
-    console.log("prepending");
+      $(".flex-outer").prepend(resultForm);
 
-    $('.flex-outer').prepend(resultForm);
+      console.log("done");
 
-    console.log("done");
+      const textBox = document.getElementById("item-title");
+      const descriptionBox = document.getElementById("message");
+      descriptionBox.value = description;
+      textBox.value = item;
 
-    const textBox = document.getElementById("item-title");
-    const descriptionBox = document.getElementById("message");
-    descriptionBox.value = description;
-    textBox.value = item;
+      $.ajax({
+        url: `/api/categories/${cId}`,
+        type: "GET",
+      })
+        .done((response) => {
+          let categories = [];
 
+          for (let id of response) {
+            categories.push(id.title);
+          }
 
-    $.ajax({
-      url: `/api/categories/${cId}`,
-      type: 'GET',
-    }).done((response) => {
-      let categories = [];
+          console.log("categories", categories);
 
-      for (let id of response) {
-        categories.push(id.title);
+          const toEatCheckbox = document.getElementById("to-eat");
+          const toReadCheckbox = document.getElementById("to-read");
+          const toWatchCheckbox = document.getElementById("to-watch");
+          const toBuyCheckbox = document.getElementById("to-buy");
+
+          toEatCheckbox.checked = false;
+          toReadCheckbox.checked = false;
+          toWatchCheckbox.checked = false;
+          toBuyCheckbox.checked = false;
+
+          if (categories.includes("To Eat")) {
+            toEatCheckbox.checked = true;
+          }
+
+          if (categories.includes("To Watch")) {
+            toWatchCheckbox.checked = true;
+          }
+
+          if (categories.includes("To Read")) {
+            toReadCheckbox.checked = true;
+          }
+
+          if (categories.includes("To Buy")) {
+            toBuyCheckbox.checked = true;
+          }
+
+          clearInterval(intervalId);
+          //$('#item-title').text(`${item} : ${categories}`);
+          console.log("item", item);
+        })
+        .fail((xhr, status, error) => {
+          console.error("Error occurred in GET /api/categories: ", error);
+          clearInterval(intervalId);
+          $("#item-title").text("Error occurred");
+        });
+    })
+    .fail((xhr, status, error) => {
+      if (error === "abort") {
+        console.log("Request aborted");
+        $("#item-title").text("Request Timed Out. Please try again");
+      } else {
+        console.error("Error occurred in POST /api/items: ", error);
+        clearInterval(intervalId);
+        $("#item-title").text("Error occurred");
       }
-
-      console.log("categories", categories);
-
-      const toEatCheckbox = document.getElementById("to-eat");
-      const toReadCheckbox = document.getElementById("to-read");
-      const toWatchCheckbox = document.getElementById("to-watch");
-      const toBuyCheckbox = document.getElementById("to-buy");
-
-      toEatCheckbox.checked = false;
-      toReadCheckbox.checked = false;
-      toWatchCheckbox.checked = false;
-      toBuyCheckbox.checked = false;
-
-      if (categories.includes('To Eat')) {
-        toEatCheckbox.checked = true;
-      }
-
-      if (categories.includes('To Watch')) {
-        toWatchCheckbox.checked = true;
-      }
-
-      if (categories.includes('To Read')) {
-        toReadCheckbox.checked = true;
-      }
-
-      if (categories.includes('To Buy')) {
-        toBuyCheckbox.checked = true;
-      }
-
-      clearInterval(intervalId);
-      //$('#item-title').text(`${item} : ${categories}`);
-      console.log("item", item);
-
-    }).fail((xhr, status, error) => {
-      console.error("Error occurred in GET /api/categories: ", error);
-      clearInterval(intervalId);
-      $('#item-title').text('Error occurred');
     });
-
-  }).fail((xhr, status, error) => {
-    if (error === 'abort') {
-      console.log('Request aborted');
-      $('#item-title').text('Request Timed Out. Please try again');
-    } else {
-      console.error("Error occurred in POST /api/items: ", error);
-      clearInterval(intervalId);
-      $('#item-title').text('Error occurred');
-    }
-  });
 
   // After five seconds, abort the request
   setTimeout(() => {
@@ -185,50 +182,55 @@ const aiForm = (formData) => {
 };
 
 $(document).ready(() => {
-  console.log('ready!');
-  const textbox = $('#itemTextBox');
+  console.log("ready!");
+  const textbox = $("#itemTextBox");
   textbox.val("Please log in");
-  textbox.prop('disabled', true);
-
+  textbox.prop("disabled", true);
+  const userName = localStorage.getItem("userName");
+  if (userName) {
+    const welcomeMessage = $("#welcomeMessage");
+    welcomeMessage.text(`Welcome, ${userName}!`);
+    localStorage.removeItem("userName"); // Remove the stored user name
+  }
 
   $.ajax({
-    url: '/users/check-authentication',
-    method: 'GET',
+    url: "/users/check-authentication",
+    method: "GET",
     xhrFields: {
-      withCredentials: true // Send cookies along with the request
+      withCredentials: true, // Send cookies along with the request
     },
-    success: function(data) {
+    success: function (data) {
       if (data.authenticated) {
         console.log("authenticated");
-        textbox.prop('disabled', false); // Enable the textbox if the user is logged in
+        textbox.prop("disabled", false); // Enable the textbox if the user is logged in
         textbox.val("");
-        textbox.attr('placeholder', 'Please enter what you would like to add to your todo list');
-        $('#loginButton').addClass('hidden');
-        $('#logoutButton').removeClass('hidden');
-
-
+        textbox.attr(
+          "placeholder",
+          "Please enter what you would like to add to your todo list"
+        );
+        $("#loginButton").addClass("hidden");
+        $("#logoutButton").removeClass("hidden");
       }
     },
-    error: function(error) {
-      console.error('Error:', error);
-    }
+    error: function (error) {
+      console.error("Error:", error);
+    },
   });
 
   $.ajax({
-    url: '/api/items',
-    method: 'GET',
-    dataType: 'json',
+    url: "/api/items",
+    method: "GET",
+    dataType: "json",
   })
-    .done(function(response) {
+    .done(function (response) {
       console.log(response);
       populateTable(response);
     })
-    .fail(function(error) {
-      console.log('Error:', error);
+    .fail(function (error) {
+      console.log("Error:", error);
     });
 
-
-  $('#item-form').submit(function(event) {
+  $("#item-form").submit(function (event) {
     event.preventDefault();
 
     const $form = $(this);
@@ -236,89 +238,84 @@ $(document).ready(() => {
 
     aiForm(formData);
 
-    $('.container').slideDown();
+    $(".container").slideDown();
   });
 
-  $('#userInteraction').submit(function(event) {
+  $("#userInteraction").submit(function (event) {
     event.preventDefault();
 
     const $form = $(this);
     const formData = $form.serialize();
 
-
-    console.log("formdata",$form);
-
+    console.log("formdata", $form);
 
     const ajaxRequest = $.ajax({
-      url: '/api/items',
-      type: 'POST',
+      url: "/api/items",
+      type: "POST",
       data: formData,
     }).done((response) => {
-
       // $('.container').slideUp();
 
+      console.log("form repose: ", response);
 
-      console.log("form repose: ",response);
-
-
-      $('.container').slideUp(function() {
+      $(".container").slideUp(function () {
         const selector = document.getElementById("flex-outer");
         if (selector) {
           selector.remove();
         }
 
-        $('#userInteraction ').prepend(`      <ul class="flex-outer initial-outer" id="flex-outer">
+        $(
+          "#userInteraction "
+        ).prepend(`      <ul class="flex-outer initial-outer" id="flex-outer">
         <p id="loading-ai">Loading from AI</p>
       </ul>`);
 
         $.ajax({
-          url: '/api/items',
-          method: 'GET',
-          dataType: 'json',
+          url: "/api/items",
+          method: "GET",
+          dataType: "json",
         })
-          .done(function(response) {
-            console.log("response ",response);
+          .done(function (response) {
+            console.log("response ", response);
             $("tbody tr").remove();
 
             populateTable(response);
           })
-          .fail(function(error) {
-            console.log('Error:', error);
+          .fail(function (error) {
+            console.log("Error:", error);
           });
       });
-
-
     });
-
   });
 
-  $('#registrationForm').submit(function(event) {
+  $("#registrationForm").submit(function (event) {
     event.preventDefault();
     showRegistrationSuccessMessage();
   });
 
-  $('#logoutButton').on('click', logout);
+  $("#logoutButton").on("click", logout);
 
-
-  $('#loginForm').submit(function(event) {
+  $("#loginForm").submit(function (event) {
     event.preventDefault();
 
     const $form = $(this);
     const formData = $form.serialize();
 
+    performLogin(formData)
+      .then((result) => {
+        // const welcomeMessage = $("#welcomeMessage");
+        // welcomeMessage.text(`Welcome, ${result.user.name}!`);
+        const userName = result.user.name;
 
-    performLogin(formData).then(result => {
-      const welcomeMessage = $("#welcomeMessage");
-      welcomeMessage.text(`Welcome, ${result.user.name}!`);
+        localStorage.setItem("userName", userName);
 
-      $("#error-message").remove();
-      location.reload();
+        $("#error-message").remove();
+        modal.style.display = "none";
 
-    })
-    .catch(error => {
-      showLoginErrorMessage(error);
-    });
+        location.reload();
+      })
+      .catch((error) => {
+        showLoginErrorMessage(error);
+      });
   });
-
 });
-
