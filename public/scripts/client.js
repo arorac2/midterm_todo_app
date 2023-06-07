36153c81fd5959fec97a8daae6f8a5878414d045
Darragh Murphy <dmurphy@servuo.com>
2023-06-07 16:30:32 -0700

    Show items on the table.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,12 +3,11 @@ $(document).ready(function() {
 
   const populateTable = function(data) {
     const tableBody = $('tbody');
-    tableBody.empty();
 
     for (const item of data) {
       const row = `
         <tr>
-          <td>${textarea1}</td>
+          <td>${item.id}</td>
           <td>${item.title}</td>
           <td>${item.description}</td>
           <td>${item.created_at}</td>
@@ -21,20 +20,20 @@ $(document).ready(function() {
     }
   };
 
-  const fetchData = function() {
-    $.ajax({
-      url: '/???', // what url we need to use?
-      method: 'GET',
-      dataType: 'json',
-    })
-      .done(function(response) {
-        console.log(response);
-        populateTable(response);
-      })
-      .fail(function(error) {
-        console.log('Error:', error);
-      });
-  };
 
-  fetchData();
+
+  $.ajax({
+    url: '/api/items', // what url we need to use?
+    method: 'GET',
+    dataType: 'json',
+  })
+    .done(function(response) {
+      console.log(response);
+      populateTable(response);
+    })
+    .fail(function(error) {
+      console.log('Error:', error);
+    });
+
+
 });
