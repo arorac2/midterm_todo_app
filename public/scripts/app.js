e2c5d214b73f8678ce318219b3e414b8e637b756
Darragh Murphy <dmurphy@servuo.com>
2023-06-08 13:49:49 -0700

    Fix item box submission.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -42,8 +42,7 @@ const aiForm = (formData) => {
 
   const resultForm = `
   <div class="container">
-    <form id="userInteraction">
-      <ul class="flex-outer">
+      <ul class="flex-outer" id="flex-outer">
         <li>
           <label for="item-title">Item Name</label>
           <input type="text" id="item-title" name="title">
@@ -77,9 +76,10 @@ const aiForm = (formData) => {
           <button type="submit">Submit</button>
         </li>
       </ul>
-    </form>
   </div>
   `;
+
+  const loadingMessage = `<p id="loading-ai">Loading from AI</p>`;
 
   const intervalId = setInterval(() => {
     // textBox.value.append('.');
@@ -97,11 +97,19 @@ const aiForm = (formData) => {
     const cId = response['categoryId'];
     console.log("cid1: ", cId);
 
-    $('#loading-ai').remove();
+    $('.initial-outer').remove();
 
     console.log("prepending");
 
-    $('.flex-outer').prepend(resultForm);
+    const selector = document.getElementById("flex-outer");
+
+    if (selector) {
+      selector.remove();
+    }
+
+
+
+    $('#userInteraction').prepend(resultForm);
 
     console.log("done");
 
@@ -148,6 +156,7 @@ const aiForm = (formData) => {
       if (categories.includes('To Buy')) {
         toBuyCheckbox.checked = true;
       }
+
 
       clearInterval(intervalId);
       //$('#item-title').text(`${item} : ${categories}`);
@@ -217,7 +226,16 @@ $(document).ready(() => {
       type: 'POST',
       data: formData,
     }).done((response) => {
-      $('.container').slideUp();
+      $('.container').slideUp(function() {
+        const selector = document.getElementById("flex-outer");
+        if (selector) {
+          selector.remove();
+        }
+
+        $('#userInteraction ').prepend(`      <ul class="flex-outer initial-outer" id="flex-outer">
+        <p id="loading-ai">Loading from AI</p>
+      </ul>`);
+      });
 
     });
 
@@ -244,7 +262,7 @@ $(document).ready(() => {
     });
   });
 
+
+
 });
-
-
 
